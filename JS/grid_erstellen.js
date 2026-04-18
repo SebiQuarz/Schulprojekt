fa340c217fc3e93372b210a49bfe297edc26850a
PauRVOH <paursiu@proton.me>
2026-04-18 19:58:43 +0200

    Bilderordner + Flagge an/aus

--- JS/grid_erstellen.js
+++ JS/grid_erstellen.js
@@ -36,7 +36,7 @@ function spielfeld_erstellen(){
             let Feld = document.createElement("button");
             Feld.style.width = "30px";
             Feld.style.height = "30px";
-            Feld.style.backgroundColor = "#434343";
+            Feld.style.backgroundColor = "rgb(169, 169, 169)";
             Feld.style.border = "1px solid #ffffff";
             grid.appendChild(Feld);
             Feld.classList.add("Feld-btn");
@@ -71,9 +71,16 @@ minen_wo()
 
 
 function right_click(Feld){
-    Feld.style.backgroundColor = "#ff0000";
+    if (Feld.style.backgroundImage.includes('../Bilder/flagge.png')){
+        Feld.style.backgroundImage = "none";
+    } else {  
+        Feld.style.backgroundImage = "url('../Bilder/flagge.png')";
+        Feld.style.backgroundSize = "cover";
+    }
 }
 
 function left_click(Feld){
     Feld.style.backgroundColor = "#1ef3cc";
 }
+
+
